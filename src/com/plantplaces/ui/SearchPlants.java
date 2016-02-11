6f6c61fc5c63a92fad8dac9354fc37e92efeee6d
Tesfagabir <tesfagabir2004@yahoo.com>
2016-02-11 12:22:10 +0400

    Accessing Spring Session ManagedBean from other SMB in JSF

--- src/com/plantplaces/ui/SearchPlants.java
+++ src/com/plantplaces/ui/SearchPlants.java
@@ -1,16 +1,35 @@
 package com.plantplaces.ui;
 
 import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.springframework.context.annotation.Scope;
+
+import com.plantplaces.dto.Plant;
 
 @Named
 @ManagedBean
 @Scope("session")
 public class SearchPlants {
 
-	public String execute(){
-		return "noresults";
+	@Inject
+	private Plant plant;
+
+	public String execute() {
+		if (plant != null && plant.getName().equalsIgnoreCase("Redbud")) {
+			return "search";
+			
+		} else {
+			return "noresults";
+		}
+	}
+
+	public Plant getPlant() {
+		return plant;
+	}
+
+	public void setPlant(Plant plant) {
+		this.plant = plant;
 	}
 }
