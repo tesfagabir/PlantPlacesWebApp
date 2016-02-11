6a115ec95ba078a9dd1fa96cd1db8671c49aa58a
Tesfagabir <tesfagabir2004@yahoo.com>
2016-02-11 17:54:19 +0400

    Added a PrimeFaces autoComplete widget to JSF page

--- src/com/plantplaces/dto/Plant.java
+++ src/com/plantplaces/dto/Plant.java
@@ -19,5 +19,10 @@ public class Plant {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
 
 }
--- src/com/plantplaces/ui/SearchPlants.java
+++ src/com/plantplaces/ui/SearchPlants.java
@@ -1,5 +1,8 @@
 package com.plantplaces.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -31,5 +34,25 @@ public class SearchPlants {
 
 	public void setPlant(Plant plant) {
 		this.plant = plant;
+	}
+	
+	public List<Plant> completePlants(String query){
+		List<Plant> plants = new ArrayList<Plant>();
+		
+		//Create plants and add them to the collection.
+		Plant redbud = new Plant();
+		redbud.setName("Eastern Redbud");
+		plants.add(redbud);
+		
+		Plant pawpaw = new Plant();
+		pawpaw.setName("Pawpaw");
+		plants.add(pawpaw);
+		
+		Plant nasturtium = new Plant();
+		nasturtium.setName("Nasturtium");
+		plants.add(nasturtium);
+		
+		return plants;
+		
 	}
 }
