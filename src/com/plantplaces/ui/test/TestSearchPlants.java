d8c5cc1bc8af17368f116b6feed3834caa8b4577
Tesfagabir <tesfagabir2004@yahoo.com>
2016-02-12 15:54:24 +0400

    EclEmma in Eclipse for code coverage and get rid of red ifs

---
+++ src/com/plantplaces/ui/test/TestSearchPlants.java
@@ -0,0 +1,67 @@
+package com.plantplaces.ui.test;
+
+import org.junit.Test;
+
+import com.plantplaces.dto.Plant;
+import com.plantplaces.ui.SearchPlants;
+
+import junit.framework.TestCase;
+
+public class TestSearchPlants extends TestCase {
+
+	private SearchPlants searchPlants;
+	private String execute;
+
+	@Test
+	public void testSearchPlantsExecute() {
+		givenSearchPlantsCreatedWithRedbud();
+		whenInvokeExecute();
+		thenVerifyReturnSearch();
+	}
+	
+	@Test
+	public void testSearchPlantsWithNoRedbud(){
+		givenSearchPlantsCreatedWithoutRedbud();
+		whenInvokeExecute();
+		thenVerifyReturnNoResults();
+	}
+	
+	@Test
+	public void testSearchPlantsWithNullPlant(){
+		givenSearchPlantsCreatedWithNullPlant();
+		whenInvokeExecute();
+		thenVerifyReturnNoResults();
+	}
+
+	private void givenSearchPlantsCreatedWithNullPlant() {
+		searchPlants = new SearchPlants();
+	}
+
+	private void givenSearchPlantsCreatedWithoutRedbud() {
+		searchPlants = new SearchPlants();
+		Plant plant = new Plant();
+		plant.setName("Pawpaw");
+		searchPlants.setPlant(plant);
+	}
+
+	private void thenVerifyReturnNoResults() {
+		assertEquals("noresults", execute);
+	}
+
+	private void givenSearchPlantsCreatedWithRedbud() {
+		searchPlants = new SearchPlants();
+		Plant plant = new Plant();
+		plant.setName("Redbud");
+		searchPlants.setPlant(plant);
+
+	}
+
+	private void whenInvokeExecute() {
+		execute = searchPlants.execute();
+	}
+
+	private void thenVerifyReturnSearch() {
+		assertEquals("search", execute);
+	}
+
+}
